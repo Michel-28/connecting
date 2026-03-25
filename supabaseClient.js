943ee3f9986df33672e89e0cd2d35dfe4ed226a5
BALAGANI VENKATA KARTHEEK <bvkatheekbvkartheek@gmail.com>
2026-03-25 17:05:57 +0530

    clean project structure

--- supabaseClient.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://ywhpcbgcfwpyrkanhpnj.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl3aHBjYmdjZndweXJrYW5ocG5qIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ0MTMyMzYsImV4cCI6MjA4OTk4OTIzNn0.TDQnCbUqmZJCSbmZG6Zn7Fnlggv3DaEXk4hW4YG1nm4";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
